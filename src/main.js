0c0d40abf2cc908c0d3020656d50c876d95ce959
yuchen2022 <102902043+yuchen2022@users.noreply.github.com>
2023-07-17 15:50:50 +0430

    updated

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./routes.js";
-createApp(App).mount("#app");
+const app = createApp(App)
+App.mount("#app");
 
 
